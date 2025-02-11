37c25c935aa7e7475ec7f0ffe30be3aae79d2fe3
suca19 <carlos.enrique.suca@gmail.com>
2025-02-11 22:29:30 +0000

    adding day7

---
+++ day7/js/script.js
@@ -0,0 +1,32 @@
+console.log('Hello World');
+
+let name = ' Carlos';
+const age = 30;
+var isDeveloper = true; 
+
+//-Comentario ----->
+const nombre = 'Carlitos';
+
+const new_nombre = nombre.toUpperCase();
+
+console.log(new_nombre);
+
+//list with push
+const list = []
+list.push(157);
+const anotherList = list.concat[1];
+
+console.log(list[0]);
+
+console.log(anotherList);
+
+const persona = {
+    name: 'Carlos',
+    twitter: '@carlos',
+    age: 21,
+    isDeveloper: true,
+    links: ['https://carlos.com', 'https://carlitos.com']
+}
+
+console.log(persona.name);
+console.log(persona.links[1]);
